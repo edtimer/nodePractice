90ddffacd890d8e54f7d949ed99ea81071c50131
edtimer <26600954+edtimer@users.noreply.github.com>
2022-02-27 10:24:17 +0000

    fix: updated http module

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const http = require("http");
-
+//the following creates a web server, requries a call back with two params
 const server = http.createServer((req, res) => {
-  res.write("initializeing server");
-  res.sendDate();
-  res.end;
+  //specifying url
+  if (req.url === "/") {
+    res.write("initializeing server");
+    res.write("something else");
+    res.getHeaderNames();
+    res.end("worksssss");
+  } else if (req.url === "/about") {
+    res.write("in the about page");
+    res.end();
+  } else {
+    res.end(`
+  <h1 style='text-align:center;color:red'>Error cant find page</h1>
+  <br>
+  <a  href="/">Go back</a>`);
+  }
 });
+//specify the port
 server.listen(5000);
