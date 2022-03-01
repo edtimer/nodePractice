c27f3b5503f071892ae0786aabc37ea4b6af2fee
edtimer <26600954+edtimer@users.noreply.github.com>
2022-03-01 15:36:09 +0000

    feat:added nodemon for hot reload

--- app.js
+++ app.js
@@ -1,24 +1,8 @@
-const http = require("http");
-//the following creates a web server, requries a call back with two params
-const server = http.createServer((req, res) => {
-  //specifying url
-  if (req.url === "/") {
-    res.write("initializeing server");
-    res.write("something else");
-    console.log(res.getHeaderNames());
-    res.end("worksssss");
-  } else if (req.url === "/about") {
-    res.write("in the about page");
-    res.end();
-  } else {
-    res.end(`
-  <h1 style='text-align:center;color:red'>Error cant find page</h1>
-  <br>
-  <a style='
-  color: red;
-  background-color: transparent;
-  text-decoration: none;text-align:center;' href="/">Go back</a>`);
-  }
-});
-//specify the port
-server.listen(5000);
+//initializing lodash
+const _ = require("lodash");
+//using lodash to flaten an array of arrays
+const items = [1, [23, [45, 67, [3]]]];
+
+const newItems = _.flattenDeep(items);
+
+console.log(newItems);
