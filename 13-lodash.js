96b9b30c313f25aed3f28a52caf9f9dc82ff774c
edtimer <26600954+edtimer@users.noreply.github.com>
2022-03-04 12:39:29 +0000

    feat: added blocking code to test event loop

---
+++ 13-lodash.js
@@ -0,0 +1,7 @@
+const _ = require("lodash");
+//using lodash to flaten an array of arrays
+const items = [1, [23, [45, 22, 67, [3]]]];
+
+const newItems = _.flattenDeep(items);
+
+console.log(newItems);
--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-//initializing lodash
-const _ = require("lodash");
-//using lodash to flaten an array of arrays
-const items = [1, [23, [45, 22, 67, [3]]]];
+const http = require("http");
+const { reset } = require("nodemon");
 
-const newItems = _.flattenDeep(items);
-
-console.log(newItems);
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("Home page");
+  }
+  if (req.url === "/about") {
+    //blocking code!!!
+    for (let i = 0; i < 100; i++) {
+      for (let j = 0; j < 100; j++) {
+        console.log(`${i} ${j}`);
+      }
+    }
+    res.end("About page");
+  } else {
+    res.end("Error page");
+  }
+});
+const port = 5000;
+server.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
