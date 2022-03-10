84c6defcb0124e41fbf06acffca06aa934b61924
edtimer <26600954+edtimer@users.noreply.github.com>
2022-03-10 01:28:39 +0000

    convert app js to use promises with no wrapper function

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
-const { readFile, writeFile } = require("fs");
+const { readFile, writeFile } = require("fs").promises;
 
-const getText = (path) => {
-  //return a promise with a call back function in it
-  return new Promise((resolve, reject) => {
-    readFile(path, "utf-8", (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
-};
+// const getText = (path) => {
+//   //return a promise with a call back function in it
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf-8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
 //async function
+// const start = async () => {
+//   try {
+//     const first = await getText("./content/writen.txt");
+//     const second = await getText("./content/secondwritten.txt");
+//     console.log(first, second);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 const start = async () => {
   try {
-    const first = await getText("./content/writen.txt");
-    const second = await getText("./content/secondwritten.txt");
+    const first = await readFile("./content/writen.txt", "utf-8");
+    const second = await readFile("./content/secondwritten.txt", "utf-8");
+    await writeFile("./content/secondwritten.txt", `this works!: ${first} `, {
+      flag: `a`,
+    });
     console.log(first, second);
   } catch (error) {
     console.log(error);
