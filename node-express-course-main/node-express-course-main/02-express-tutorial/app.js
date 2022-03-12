7ee283e2b540fcafedb5115cd7859dccf3bb5bcf
edtimer <26600954+edtimer@users.noreply.github.com>
2022-03-12 09:51:26 +0000

    preparing for express implementation

--- node-express-course-main/node-express-course-main/02-express-tutorial/app.js
+++ node-express-course-main/node-express-course-main/02-express-tutorial/app.js
@@ -1,1 +1,11 @@
-console.log('Express Tutorial')
+console.log("Express Tutorial");
+const http = require("http");
+const server = http.createServer((req, res) => {
+  console.log("the user has hit the server");
+  res.end("Home pagels");
+});
+
+const port = 5000;
+server.listen(port, () => {
+  console.log(`listening on port ${port} `);
+});
