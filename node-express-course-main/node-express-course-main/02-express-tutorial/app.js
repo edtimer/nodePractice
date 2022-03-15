0631dc3ef5c4c027548944526341234d151130e3
edtimer <26600954+edtimer@users.noreply.github.com>
2022-03-15 04:37:09 +0000

    practice on header codes

--- node-express-course-main/node-express-course-main/02-express-tutorial/app.js
+++ node-express-course-main/node-express-course-main/02-express-tutorial/app.js
@@ -1,7 +1,9 @@
 console.log("Express Tutorial");
 const http = require("http");
 const server = http.createServer((req, res) => {
+  res.writeHead(401, { "content-type": "text-html" });
   console.log("the user has hit the server");
+  res.write("<h1>Welcome to home page</h1>");
   res.end("Home pagels");
 });
 
