64f62db2e880f18e5d7c450b1444a45fc6d0c0c2
edtimer <26600954+edtimer@users.noreply.github.com>
2022-03-15 04:45:14 +0000

    request object practice code

--- node-express-course-main/node-express-course-main/02-express-tutorial/app.js
+++ node-express-course-main/node-express-course-main/02-express-tutorial/app.js
@@ -2,9 +2,15 @@ console.log("Express Tutorial");
 const http = require("http");
 const server = http.createServer((req, res) => {
   res.writeHead(401, { "content-type": "text-html" });
-  console.log("the user has hit the server");
-  res.write("<h1>Welcome to home page</h1>");
-  res.end("Home pagels");
+  console.log(req.url);
+  if (req.url !== "/test") {
+    res.writeHead(404, { "content-type": "text-html" });
+    res.end("Not found");
+  } else {
+    console.log("the user has hit the server");
+    res.write("<h1>Welcome to home page</h1>");
+    res.end("stuff here");
+  }
 });
 
 const port = 5000;
