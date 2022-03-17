0888977949f46ab8c50469acf8ecfff2ba9dc51c
edtimer <26600954+edtimer@users.noreply.github.com>
2022-03-17 07:25:17 +0000

    added express related lines in line 26 of app js

--- node-express-course-main/node-express-course-main/02-express-tutorial/app.js
+++ node-express-course-main/node-express-course-main/02-express-tutorial/app.js
@@ -1,23 +1,36 @@
-console.log("Express Tutorial");
-const { readFileSync } = require("fs");
-const http = require("http");
-//get all files
-const homePage = readFileSync("./index.html");
-const server = http.createServer((req, res) => {
-  res.writeHead(401, { "content-type": "text-html" });
-  console.log(req.url);
-  if (req.url !== "/test") {
-    res.writeHead(404, { "content-type": "text-html" });
-    res.end("Not found");
-  } else {
-    console.log("the user has hit the server");
-    res.write("<h1>Welcome to home page</h1>");
-    res.write(homePage);
-    res.end("stuff here");
-  }
+// console.log("Express Tutorial");
+// const { readFileSync } = require("fs");
+// const http = require("http");
+// //get all files
+// const homePage = readFileSync("./index.html");
+// const server = http.createServer((req, res) => {
+//   res.writeHead(401, { "content-type": "text-html" });
+//   console.log(req.url);
+//   if (req.url !== "/test") {
+//     //changin to "content-type": "text-plain" would print the litteral html
+//     res.writeHead(404, { "content-type": "text-html" });
+//     res.end("Not found");
+//   } else {
+//     console.log("the user has hit the server");
+//     res.write("<h1>Welcome to home page</h1>");
+//     res.write(homePage);
+//     res.end("stuff here");
+//   }
+// });
+const express = require("express");
+const path = require("path");
+const app = express();
+console.log(__dirname);
+app.get("/", (req, res) => {
+  console.log("home page");
+  res.sendFile(__dirname, path.join()); // needs fixing
+});
+
+app.get("*", (req, res) => {
+  res.status(404).send("resource not found");
 });
 
 const port = 5000;
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`listening on port ${port} `);
 });
